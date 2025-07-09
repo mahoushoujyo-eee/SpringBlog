762a411bfe6da6b914b7adda018bdb9762bfdb70
mahoushoujyo-eee <1653545106@qq.com>
2025-07-09 23:02:19 +0800

    Enhance HelloController with logging and Dubbo service annotation; update application.yml to change Dubbo protocol to tri

--- account-service/src/main/java/org/eee/account/controller/HelloController.java
+++ account-service/src/main/java/org/eee/account/controller/HelloController.java
@@ -1,9 +1,13 @@
 package org.eee.account.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.dubbo.config.annotation.DubboService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
+@DubboService
 public class HelloController
 {
     @GetMapping("/user/hello")
@@ -15,6 +19,7 @@ public class HelloController
     @GetMapping("/public/hello")
     public String helloPublic()
     {
+        log.info("helloPublic");
         return "hello";
     }
 
